617a129b28679e7c58bf4a8afe15c8df85d68e26
Rubenhdz20 <rubhernandez42@gmail.com>
2025-05-17 20:40:42 -0600

    optmize Edit Invoice into more sub components

--- src/components/invoiceForms/BillFromSection.tsx
+++ src/components/invoiceForms/BillFromSection.tsx
@@ -1,2 +1,118 @@
 import React from "react";
+import { Control, Controller, FieldErrors } from 'react-hook-form'
+import { InvoiceFormValues } from '../../pages/invoices/EditInvoice'
 
+interface Props {
+  control: Control<InvoiceFormValues>
+  errors: FieldErrors<InvoiceFormValues>
+}
+
+const BillFromSection = ({ control, errors }: Props) => {
+    return (
+        <>
+            <section className="space-y-2">
+                <h2 className="mb-6 text-sm text-[#7C5DFA] font-bold uppercase">Bill From</h2>
+                <Controller
+                    control={control}
+                    name="senderAddress.street"
+                    defaultValue=""
+                    rules={{ required: "Street is required" }}
+                    render={({ field, fieldState }) => (
+                        <div>
+                            <label className={`block mb-1 text-gray-400 text-sm
+                                ${errors.senderAddress?.street ? "text-red-500" : "text-gray-400"}
+                            `}>
+                                Street Address
+                            </label>
+                            <input
+                                {...field}
+                                className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
+                                ${errors.senderAddress?.street 
+                                    ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
+                                    : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"
+                                }`}
+                            />
+                        </div>
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="senderAddress.city"
+                    defaultValue=""
+                    rules={{ required: "City is required" }}
+                    render={({ field, fieldState }) => (
+                        <div className="mt-6 space-x-2">
+                            <div>
+                                <label className={`block mb-1 text-gray-400 text-sm 
+                                    ${errors.senderAddress?.city ? "text-red-500" : "text-gray-400"}`}
+                                >
+                                    City
+                                </label>
+                                <input
+                                    {...field}
+                                    className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
+                                    ${errors.senderAddress?.street 
+                                        ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
+                                        : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
+                                    }
+                                />
+                            </div>
+                        </div>
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="senderAddress.postCode"
+                    defaultValue=""
+                    rules={{ required: "Post code is required" }}
+                    render={({ field, fieldState }) => (
+                    <div className="mt-6 space-x-2"> 
+                        <div>
+                            <label className={`block mb-1 text-gray-400 text-sm 
+                                    ${errors.senderAddress?.postCode ? "text-red-500" : "text-gray-400"}`}
+                                >
+                                Post Code
+                            </label>
+                            <input
+                                    {...field}
+                                    className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
+                                    ${errors.senderAddress?.postCode
+                                        ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
+                                        : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
+                                    }
+                                />
+                        </div>
+                    </div>
+                    )}
+                />
+                <Controller
+                    control={control}
+                    name="senderAddress.country"
+                    defaultValue=""
+                    rules={{ required: "Country is required" }}
+                    render={({ field, fieldState }) => (
+                    <div className="mt-6 space-x-2"> 
+                        <div>
+                            <label className={`block mb-1 text-gray-400 text-sm 
+                                    ${errors.senderAddress?.country ? "text-red-500" : "text-gray-400"}`}
+                                >
+                                Country
+                            </label>
+                            <input
+                                {...field}
+                                className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
+                                ${errors.senderAddress?.country
+                                    ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
+                                    : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
+                                }
+                            />
+                        </div>
+                    </div>
+                    )}
+                />
+            </section>
+        </>        
+    )
+}
+
+export default BillFromSection;
--- src/pages/invoices/EditInvoice.tsx
+++ src/pages/invoices/EditInvoice.tsx
@@ -3,10 +3,11 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useForm, useFieldArray, useWatch, Controller } from "react-hook-form";
 import { useInvoiceStore, Invoice } from "../../store/InvoiceStore";
 import { formatCurrency } from "../../utils/formatCurrency";
+import BillFromSection from "../../components/invoiceForms/BillFromSection";
 import GoBackButton from "../../components/buttons/GoBackButton";
 
 // We don’t edit id or status here, but we want status as a selectable field.
-type InvoiceFormValues = Omit<Invoice, 'id' | 'total' | 'status'>
+export type InvoiceFormValues = Omit<Invoice, 'id' | 'total' | 'status'>
 & { status: Invoice['status'] };
 
 
@@ -94,108 +95,7 @@ const EditInvoice: React.FC = () => {
             <form id="edit-invoice-form" onSubmit={handleSubmit(onSubmit)} className="min-h-screen p-6 pb-20 space-y-6 bg-[#141625]">
                 <GoBackButton/>
                 <h1 className="text-2xl font-bold text-white">Edit <span className="text-[#777F98]">#</span>{invoice.id}</h1>
-                {/* // Bill FROM Section  */}
-                <section className="space-y-2">
-                    <h2 className="mb-6 text-sm text-[#7C5DFA] font-bold uppercase">Bill From</h2>
-                    <Controller
-                        control={control}
-                        name="senderAddress.street"
-                        defaultValue={invoice?.senderAddress.street ?? ""}
-                        rules={{ required: "Street is required" }}
-                        render={({ field, fieldState }) => (
-                            <div>
-                                <label className={`block mb-1 text-gray-400 text-sm
-                                    ${errors.senderAddress?.street ? "text-red-500" : "text-gray-400"}
-                                `}>
-                                    Street Address
-                                </label>
-                                <input
-                                    {...field}
-                                    className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
-                                    ${errors.senderAddress?.street 
-                                        ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
-                                        : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"
-                                    }`}
-                                />
-                            </div>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name="senderAddress.city"
-                        defaultValue={invoice?.senderAddress.city ?? ""}
-                        rules={{ required: "City is required" }}
-                        render={({ field, fieldState }) => (
-                            <div className="mt-6 space-x-2">
-                                <div>
-                                    <label className={`block mb-1 text-gray-400 text-sm 
-                                        ${errors.senderAddress?.city ? "text-red-500" : "text-gray-400"}`}
-                                    >
-                                        City
-                                    </label>
-                                    <input
-                                        {...field}
-                                        className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
-                                        ${errors.senderAddress?.street 
-                                            ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
-                                            : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name="senderAddress.postCode"
-                        defaultValue={invoice?.senderAddress.postCode ?? ""}
-                        rules={{ required: "Post code is required" }}
-                        render={({ field, fieldState }) => (
-                        <div className="mt-6 space-x-2"> 
-                            <div>
-                                <label className={`block mb-1 text-gray-400 text-sm 
-                                        ${errors.senderAddress?.postCode ? "text-red-500" : "text-gray-400"}`}
-                                    >
-                                    Post Code
-                                </label>
-                                <input
-                                        {...field}
-                                        className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
-                                        ${errors.senderAddress?.postCode
-                                            ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
-                                            : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
-                                        }
-                                    />
-                            </div>
-                        </div>
-                        )}
-                    />
-                    <Controller
-                        control={control}
-                        name="senderAddress.country"
-                        defaultValue={invoice?.senderAddress.country ?? ""}
-                        rules={{ required: "Country is required" }}
-                        render={({ field, fieldState }) => (
-                        <div className="mt-6 space-x-2"> 
-                            <div>
-                                <label className={`block mb-1 text-gray-400 text-sm 
-                                        ${errors.senderAddress?.country ? "text-red-500" : "text-gray-400"}`}
-                                    >
-                                    Country
-                                </label>
-                                <input
-                                        {...field}
-                                        className={`w-full px-6 py-5 text-md bg-[#1E2139] text-white font-bold rounded border-2 border-transparent focus:border-[#252945] focus:ring-2 focus:ring-[#252945]/50 outline-none transition focus:placeholder-opacity-50 caret-[#7C5DFA]
-                                        ${errors.senderAddress?.country
-                                            ? "border-red-500 focus:border-red-500 focus:ring-red-500/40"
-                                            : "border-transparent focus:border-[#7C5DFA] focus:ring-[#7C5DFA]/40"}`
-                                        }
-                                    />
-                            </div>
-                        </div>
-                        )}
-                    />
-                </section>
+                <BillFromSection control={control} errors={errors} />
                 {/* // Bill TO Section  */}
                 <section className="space-y-2">
                     <h2 className="mb-6 text-sm text-[#7C5DFA] font-bold uppercase">Bill To</h2>
