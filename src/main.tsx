5af3718d3bff9fae49be656e60793b1adb2538c1
Rubenhdz20 <rubhernandez42@gmail.com>
2025-07-17 12:46:04 -0600

    updated env file

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,10 @@ import ReactDOM from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const frontendApi   = import.meta.env.VITE_CLERK_FRONTEND_API;
 
-if (!PUBLISHABLE_KEY) {
+if (!PUBLISHABLE_KEY || !frontendApi) {
   throw new Error('Missing Publishable Key');
 }
 
